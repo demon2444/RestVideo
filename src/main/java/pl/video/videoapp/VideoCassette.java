95de111da2d00fdd2b8b486608dbca43735d5352
Marcin Osuchowski <demon2444@gmail.com>
2020-02-05 01:26:39 +0100

    Creating entity

--- src/main/java/pl/video/videoapp/VideoCassette.java
+++ src/main/java/pl/video/videoapp/VideoCassette.java
@@ -1,9 +1,16 @@
 package pl.video.videoapp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 public class VideoCassette {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private LocalDate productionYear;
